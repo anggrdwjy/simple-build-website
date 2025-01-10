13c760573ddc60c36d51a5cf80eca4d9d41907e8
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-01-11 01:30:34 +0700

    Update web-install.sh

--- web-install.sh
+++ web-install.sh
@@ -7,7 +7,6 @@ sudo apt install mysql-server -y
 echo -n "Password Root Your Database: ";
 read passmysql
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$passmysql';" 
-sudo apt install phpmyadmin -y
 sudo apt install zip unzip php-zip
 systemctl restart apache2
 wget https://wordpress.org/latest.zip
