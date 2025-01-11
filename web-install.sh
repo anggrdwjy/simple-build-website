c471e9831bdde39cddc62e6a50a653ea051a48a9
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-01-11 16:11:36 +0700

    Update web-install.sh

--- web-install.sh
+++ web-install.sh
@@ -11,6 +11,8 @@ mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY
 sudo apt install php php-cli php-zip libapache2-mod-php php-mysql -y
 mv /etc/apache2/mods-enabled/dir.conf dir.conf.bak
 cp support/dir.conf /etc/apache2/mods-enabled/
+mv /etc/php/8.3/apache2/php.ini php.ini.bak
+cp support/php.ini /etc/php/8.3/apache2/php.ini
 systemctl restart apache2
 sudo apt install phpmyadmin -y
 wget https://wordpress.org/latest.zip
@@ -20,8 +22,8 @@ chmod o+w /var/www/html/blog
 chown www-data:www-data -R /var/www/html/blog/
 echo -n "Input Your Domain: ";
 read domain
-cp support/ssldomain.pem /etc/apache2/ssl/domain.pem
-cp support/ssldomain.key /etc/apache2/sss/domain.key
+cp support/ssldomain.pem /etc/apache2/ssl/
+cp support/ssldomain.key /etc/apache2/ssl/
 cp support/apache2.conf /etc/apache2/
 cp support/domain.conf /etc/apache2/sites-available/$domain.conf
 sudo a2ensite $domain.conf
