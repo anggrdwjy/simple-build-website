6b0ed87ca6123f00d1450939559c263c88c174ee
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-01-11 20:31:07 +0700

    Create customdomain.sh

---
+++ customdomain.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo -n "Your Domain: ";
+read domain
+cp support/custom-domain.conf /etc/apache2/sites-available/$domain.conf
+cp support/custom-ports.conf /etc/apache2/ports.conf
+nano /etc/apache2/ssl/domain.pem
+nano /etc/apache2/ssl/domain.key
+a2ensite $domain.conf
+a2dissite 000-default.conf
+a2enmod rewrite
+a2enmod ssl
+systemctl restart apache2
