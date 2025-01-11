368a9e9a2fe3c8723cf3e045834bf129e4b773ac
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-01-11 20:36:24 +0700

    Update virtualhost.sh

--- virtualhost.sh
+++ virtualhost.sh
@@ -2,8 +2,8 @@
 echo -n "Your Domain: ";
 read domain
 cp support/domain.conf /etc/apache2/sites-available/$domain.conf
-nano /etc/apache2/ssl/domain.pem
-nano /etc/apache2/ssl/domain.key
+nano /etc/apache2/ssl/$domain/domain.pem
+nano /etc/apache2/ssl/$domain/domain.key
 a2ensite $domain.conf
 a2dissite 000-default.conf
 systemctl restart apache2
