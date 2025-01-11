5c4fc84ccd0b4d2d8e025c158256f5388a53bff3
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-01-11 16:19:33 +0700

    Update web-install.sh

--- web-install.sh
+++ web-install.sh
@@ -12,7 +12,7 @@ sudo apt install php php-cli php-zip libapache2-mod-php php-mysql -y
 mv /etc/apache2/mods-enabled/dir.conf dir.conf.bak
 cp support/dir.conf /etc/apache2/mods-enabled/
 mv /etc/php/8.3/apache2/php.ini php.ini.bak
-cp support/php.ini /etc/php/8.3/apache2/php.ini
+cp support/php.ini /etc/php/8.3/apache2
 systemctl restart apache2
 sudo apt install phpmyadmin -y
 wget https://wordpress.org/latest.zip
@@ -22,8 +22,13 @@ chmod o+w /var/www/html/blog
 chown www-data:www-data -R /var/www/html/blog/
 echo -n "Input Your Domain: ";
 read domain
+a2enmod ssl
+mkdir /etc/apache2/ssl
+chmod -R 777 /etc/apache2/ssl
 cp support/ssldomain.pem /etc/apache2/ssl/
+touch /etc/apache2/ssl/ssldomain.pem
 cp support/ssldomain.key /etc/apache2/ssl/
+touch /etc/apache2/ssl/ssldomain.key
 cp support/apache2.conf /etc/apache2/
 cp support/domain.conf /etc/apache2/sites-available/$domain.conf
 sudo a2ensite $domain.conf
