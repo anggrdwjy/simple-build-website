b928570e15382aec2cd802a2ed3e6343e9b71913
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-01-11 15:33:49 +0700

    Update web-install.sh

--- web-install.sh
+++ web-install.sh
@@ -4,13 +4,13 @@ sudo apt install software-properties-common -y
 sudo apt install zip unzip -y
 sudo apt install apache2 -y
 sudo apt install mysql-server -y
-echo -n "Password Root Your Database: ";
+echo -n "Password Your Database: ";
 read passmysql
 mysql -e "CREATE DATABASE wordpress;" 
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$passmysql';"
 sudo apt install php php-cli php-zip libapache2-mod-php php-mysql -y
 mv /etc/apache2/mods-enabled/dir.conf dir.conf.bak
-cp support/dir.conf /etc/apache2/mods-enabled/dir.conf
+cp support/dir.conf /etc/apache2/mods-enabled/
 systemctl restart apache2
 sudo apt install phpmyadmin -y
 wget https://wordpress.org/latest.zip
@@ -18,3 +18,10 @@ unzip latest.zip
 mv wordpress /var/www/html/blog/
 chmod o+w /var/www/html/blog
 chown www-data:www-data -R /var/www/html/blog/
+echo -n "Input Your Domain: ";
+read domain
+cp support/apache2.conf /etc/apache2/
+cp support/domain.conf /etc/apache2/sites-available/$domain.conf
+sudo a2ensite $ndomain.conf
+sudo a2dissite 000-default
+systemctl restart apache2
