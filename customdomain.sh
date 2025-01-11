6e12c41b7781a220021cc84684bca7af2a9dae17
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-01-11 20:37:06 +0700

    Update customdomain.sh

--- customdomain.sh
+++ customdomain.sh
@@ -3,8 +3,8 @@ echo -n "Your Domain: ";
 read domain
 cp support/custom-domain.conf /etc/apache2/sites-available/$domain.conf
 cp support/custom-ports.conf /etc/apache2/ports.conf
-nano /etc/apache2/ssl/domain.pem
-nano /etc/apache2/ssl/domain.key
+nano /etc/apache2/ssl/$domain/domain.pem
+nano /etc/apache2/ssl/$domain/domain.key
 a2ensite $domain.conf
 a2dissite 000-default.conf
 a2enmod rewrite
