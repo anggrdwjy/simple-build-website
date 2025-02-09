fb96868e4f0ec3494f8409b545020bcb494c1f4d
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-02-09 22:24:48 +0700

    Update webinstall.sh

--- webinstall.sh
+++ webinstall.sh
@@ -5,19 +5,24 @@ sudo apt install zip unzip -y
 sudo timedatectl set-timezone Asia/Jakarta 
 sudo timedatectl set-ntp on
 sudo apt install apache2 -y
+cp /etc/apache2/apache2.conf apache2.conf.bak
+cp support/apache2.conf /etc/apahce2/
+systemctl restart apache2
 sudo apt install mysql-server -y
 echo -n "Password Your Database: ";
 read passmysql
 mysql -e "CREATE DATABASE wordpress;" 
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$passmysql';"
 sudo apt install php php-cli php-zip libapache2-mod-php php-mysql -y
-mv /etc/apache2/mods-enabled/dir.conf dir.conf.bak
+cp /etc/apache2/mods-enabled/dir.conf dir.conf.bak
 cp support/dir.conf /etc/apache2/mods-enabled/
-mv /etc/php/8.3/apache2/php.ini php.ini.bak
-cp support/php.ini /etc/php/8.3/apache2
+cp /etc/php/*/cli/php.ini cli-php.ini.bak
+cp /etc/php/*/apache2/php.ini apache-php.ini.bak
+cp support/php.ini /etc/php/*/cli/
+cp support/php.ini /etc/php/*/apache2/
 systemctl restart apache2
-mv /var/www/html/index.html index.html.bak
-cp support/html-index.html /var/www/html/index.html
+cp /var/www/html/index.html index.html.bak
+cp support/index.html /var/www/html/
 wget https://wordpress.org/latest.zip && unzip latest.zip
 mv wordpress /var/www/html/blog
 chown $USER:$USER -R /var/www/html/blog
